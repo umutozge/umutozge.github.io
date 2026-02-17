645199c5f0c8be173a5433bb1ef67b4294fd2836
Umut Özge <umut.oezge@gmail.com>
2026-02-17 11:07:21 +0300

    notes

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -162,7 +162,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Language primer",
-        "excerpt":"This is a quick start for cognitive science students on the cognitive science of language. A simple formal system Douglas Hofstadter1 has this tiny formal system called pq-System. In defining formal systems, we start with a set of symbols, called the alphabet. This dictates that any expression of the system...","categories": ["course"],
+        "excerpt":"This is a quick start for cognitive science students on language. A simple formal system Douglas Hofstadter1 has this tiny formal system called pq-System. In defining formal systems, we start with a set of symbols, called the alphabet. This dictates that any expression of the system must be made by...","categories": ["course"],
         "tags": ["language","cogsci"],
         "url": "/posts/course/language-primer/",
         "teaser": null
@@ -205,6 +205,9 @@ var store = [{
   },{
     "title": "Posts by Year",
     "excerpt":" ","url": "https://umutozge.github.io/posts/"
+  },{
+    "title": "The great number crunch",
+    "excerpt":"        {{ page.title }}        ","url": "https://umutozge.github.io/p/yang-crunch/"
   },{
     "title": "No Free Lunch in Linguistics Or Machine Learning: Response to Pater",
     "excerpt":"        {{ page.title }}        ","url": "https://umutozge.github.io/p/rawski-heins/"
